edaeafde85bc853856b1a168472262e1ac29dc45
andela-batolagbe <atolagbe.bisoye@andela.co>
2015-09-01 09:04:18 +0100

    Style(invertedIndex): Add comments for code explanation

--- jasmine/src/invertedIndex.js
+++ jasmine/src/invertedIndex.js
@@ -1,21 +1,24 @@
-//variable declaration
+/* This file require running it on HTTP Server
+for the JQuery AJAX file reading functionality to 
+work */
 
-
+//variables declaration
 var index = [],
   result = [],
   holder = [],
   len, i, temp = [],
   indexArray = [];
 
-
-//var fileRef = 'books.json';
-
+var fileRef = 'books.json';
 
 
-var getIndex = function() {
+//main get index function
+var getIndex = function(file, stringKeys){
 
+//getIndex.prototype.indexList(file, stringKeys);
 };
 
+//function to read file contents
 getIndex.prototype.readTexts = function(obj) {
 
   //iterate trough JSON properties and push sentences to holding array
@@ -33,27 +36,34 @@ getIndex.prototype.readTexts = function(obj) {
 
 };
 
+//function for creating index
 getIndex.prototype.createIndex = function(file) {
 
+  //call the readTexts function to read contents
   var holder = getIndex.prototype.readTexts(file);
 
-  //iterate through holder, split by whitespace and push words to result
+  //iterate through holder, split by all punctuations and push words to result
   for (i = 0; i < holder.length; i++) {
     var temp = [];
     var words = holder[i].split(/[\s\,\.\:]/);
+
+  // push words to index array
     for (var j = 0; j < words.length; j++) {
       temp.push(words[j]);
     }
     index.push(temp);
   }
-  //console.log(holder);
   return index;
 };
 
+// index search function
 getIndex.prototype.searchIndex = function(files, stringKeys) {
+  
+  //check file type if its an array, variable or file path
   var file;
   if (typeof files === typeof '') {
-    var holding = [];
+  
+    //syncronous ajax request to read file if it is a file path
     $.ajax({
       url: files,
       async: false,
@@ -63,18 +73,27 @@ getIndex.prototype.searchIndex = function(files, stringKeys) {
       }
     });
 
-  } else {
+   } else {
     file = files;
   }
+
   var converter;
+  //check if keys is an array of strings or a string
   if (typeof stringKeys === typeof []) {
+    //join if its an array
     converter = stringKeys.join();
   } else {
     converter = stringKeys;
   }
+   //convert string to lowercase and split
   var lower = converter.toLowerCase();
   var keys = lower.split(/[\s\,\.\:]/);
+
+  //call the getIndex function 
   var index = getIndex.prototype.createIndex(file);
+
+  //loop through keys and push matching keys 
+  //with their corresponding index into results
   for (var i = 0; i < keys.length; i++) {
     for (var j = 0; j < index.length; j++) {
       if (index[j].indexOf(keys[i]) >= 0) {
@@ -87,14 +106,21 @@ getIndex.prototype.searchIndex = function(files, stringKeys) {
 
 };
 
+//function to generate the index array, main fuction to be called
+//with the parameters
 getIndex.prototype.indexList = function(files, stringKeys) {
 
+//call the search index function
 var indexes =  getIndex.prototype.searchIndex(files, stringKeys);
+
+//loop trough the search results and push the
+//index numbers to indexArray;
 for (var l = 0; l <indexes.length; l++){
     indexArray.push(indexes[l][1]);
 }
-console.log(indexes);
+
 return indexArray;
 };
+
 
 //getIndex.prototype.indexList(fileRef, ['Alice', 'FALLS', 'Lord', 'elf', 'wizard', 'Imagination']);
